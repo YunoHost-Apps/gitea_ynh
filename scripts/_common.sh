a9d9989b49a3b7943844a2e929b795a51f88d7f5
Josué Tille <josue@tille.ch>
2020-12-07 16:10:16 +0100

    Add arm64 support

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,14 +14,16 @@ SSH_PATH="$DATADIR/.ssh"
 # Detect the system architecture to download the right tarball
 # NOTE: `uname -m` is more accurate and universal than `arch`
 # See https://en.wikipedia.org/wiki/Uname
-if [ -n "$(uname -m | grep 64)" ]; then
-	architecture="x86-64"
+if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+    architecture="arm64"
+elif [ -n "$(uname -m | grep 64)" ]; then
+    architecture="x86-64"
 elif [ -n "$(uname -m | grep 86)" ]; then
-	architecture="i386"
+    architecture="i386"
 elif [ -n "$(uname -m | grep armv7)" ]; then
-	architecture="armv7"
+    architecture="armv7"
 elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="arm"
+    architecture="arm"
 else
 	ynh_die --message "Unable to detect your achitecture, please open a bug describing \
         your hardware and the result of the command \"uname -m\"." 1
