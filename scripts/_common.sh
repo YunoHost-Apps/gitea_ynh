73da923b79a660fd0f7fa0ea7620e291008b10f3
Josué Tille <josue@tille.ch>
2024-08-31 09:56:13 +0200

    Cleanup after auto patch

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,8 +10,8 @@ ssh_port="$(yunohost settings get security.ssh.ssh_port)"
 #=================================================
 
 _set_permissions() {
-    #REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chown -R "$app:$app" "$install_dir"
-    #REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chmod -R u=rwX,g=rX,o= "$install_dir"
+    chown -R "$app:$app" "$install_dir"
+    chmod -R u=rwX,g=rX,o= "$install_dir"
     chmod +x "$install_dir/gitea"
 
     chown -R "$app:$app" "$data_dir"
@@ -21,7 +21,11 @@ _set_permissions() {
                    -exec chown "$app:$app" {} \; \
                    -exec chmod u=rwX,g=rX,o= {} \;
     chmod -R u=rwX,g=,o= "$data_dir/.ssh"
+}
 
-    #REMOVEME? Assuming ynh_config_add_logrotate is called, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chown -R "$app:$app" "/var/log/$app"
-    #REMOVEME? Assuming ynh_config_add_logrotate is called, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chmod -R u=rwX,g=rX,o= "/var/log/$app"
+set_settings_default() {
+    ynh_app_setting_set_default --key=internal_token --value="$(ynh_exec_as_app "$install_dir/gitea" generate secret INTERNAL_TOKEN)"
+    ynh_app_setting_set_default --key=secret_key --value="$(ynh_exec_as_app "$install_dir/gitea" generate secret SECRET_KEY)"
+    ynh_app_setting_set_default --key=lfs_jwt_secret --value="$(ynh_exec_as_app "$install_dir/gitea" generate secret JWT_SECRET)"
+    ynh_app_setting_set_default --key=jwt_secret --value="$(ynh_exec_as_app "$install_dir/gitea" generate secret JWT_SECRET)"
 }
